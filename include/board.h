1903421c1431326528fe9c166d4323ad569e58a1
jonathan <joni7840850@gmail.com>
2024-12-05 17:13:47 -0600

    Add comment to show how to map from bitboard to board

--- include/board.h
+++ include/board.h
@@ -1,27 +1,50 @@
+/*
+ * Bitboard Representation:
+ *
+ * Board is arranged from least significant bit (LSB) to most significant bit
+ * (MSB) 64-bit representation, mapping as follows:
+ *
+ * Bit 0  (LSB) = a1
+ * Bit 1        = b1
+ * Bit 2        = c1
+ * ...
+ * Bit 7        = h1
+ * Bit 8        = a2
+ * Bit 9        = b2
+ * ...
+ * Bit 63 (MSB) = h8
+ *
+ * Indexing follows row-major order:
+ * - Lowest 8 bits (0-7)   represent first rank (row 1)
+ * - Next 8 bits (8-15)    represent second rank (row 2)
+ * - ...
+ * - Highest 8 bits (56-63) represent eighth rank (row 8)
+ */
 #ifndef BOARD_H
 #define BOARD_H
 #include <stdint.h>
 
 // Some useful constants
 #define COLS 8
+#define NUMBEROFCOLORS 2
 #define ROWS 8
-#define NUMBEROFDIFFERENTPIECES 12
-#define WHITEKING 1
-#define WHITEPAWN 0
-#define WHITEQUEEN 2
-#define WHITEBISHOP 3
-#define WHITEKNIGHT 4
-#define WHITEROOK 5
-#define BLACKPAWN 6
-#define BLACKKING 7
-#define BLACKQUEEN 8
-#define BLACKBISHOP 9
-#define BLACKKNIGHT 10
-#define BLACKROOk 11
+#define WHITE 0
+#define BLACK 1
+#define NUMBEROFDIFFERENTPIECES 6
+#define PAWN 0
+#define KING 1
+#define QUEEN 2
+#define BISHOP 3
+#define KNIGHT 4
+#define ROOK 5
 
 // Will be using a bitboard representation
 typedef uint64_t bitboard;
+typedef struct chessBoard {
+  bitboard pieces[NUMBEROFCOLORS][NUMBEROFDIFFERENTPIECES];
+} chessBoard;
 void cleanBitboard(bitboard *bb);
 void printBitboard(bitboard bb);
 void placeBitValue(int col, int row, int val, bitboard *bb);
+void initBitboards(chessBoard *chessBoard);
 #endif
