baa6da10f49e8dc9b0f1c52b3511706b678e5981
jonathan <joni7840850@gmail.com>
2024-12-07 16:40:44 -0600

    Add funciton to compute pawn attacks

--- include/attacks.h
+++ include/attacks.h
@@ -1,5 +1,6 @@
 #ifndef ATTACKS_H
 #define ATTACKS_H
 #include "../include/board.h"
+
 bitboard computePawnAttack(int row, int col, int side);
 #endif
--- src/attacks.c
+++ src/attacks.c
@@ -1,16 +1,41 @@
 #include "../include/attacks.h"
 
+// THIS CONST REPRESENT THE BOARD BELOW THEM
+const bitboard NOT_COL_0 = 18374403900871474942ULL;
+// 1 1 1 1 1 1 1 0
+// 1 1 1 1 1 1 1 0
+// 1 1 1 1 1 1 1 0
+// 1 1 1 1 1 1 1 0
+// 1 1 1 1 1 1 1 0
+// 1 1 1 1 1 1 1 0
+// 1 1 1 1 1 1 1 0
+// 1 1 1 1 1 1 1 0
+
+const bitboard NOT_COL_7 = 9187201950435737471ULL;
+// 0 1 1 1 1 1 1 1
+// 0 1 1 1 1 1 1 1
+// 0 1 1 1 1 1 1 1
+// 0 1 1 1 1 1 1 1
+// 0 1 1 1 1 1 1 1
+// 0 1 1 1 1 1 1 1
+// 0 1 1 1 1 1 1 1
+// 0 1 1 1 1 1 1 1
+
 bitboard computePawnAttack(int row, int col, int side) {
   bitboard pawnAttack = 0;
   uint64_t mask = (uint64_t)1 << (col + ROWS * row);
   // side == 1 means black
   if (side) {
-
+    if (mask & NOT_COL_7)
+      pawnAttack |= (mask >> 7);
+    if (mask & NOT_COL_0)
+      pawnAttack |= (mask >> 9);
   } else {
-    if (mask << 7)
+    if (mask & NOT_COL_0)
       pawnAttack |= (mask << 7);
-    if (mask << 9)
+    if (mask & NOT_COL_7)
       pawnAttack |= (mask << 9);
   }
+  printBitboard(pawnAttack | mask);
   return pawnAttack;
 }
