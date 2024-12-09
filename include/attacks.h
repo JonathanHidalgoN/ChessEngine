6b86b401a847fdf4e63a95ee38cb13eae5b0fec7
jonathan <joni7840850@gmail.com>
2024-12-09 09:01:14 -0600

    Add pawn attacks

--- include/attacks.h
+++ include/attacks.h
@@ -1,6 +1,7 @@
 #ifndef ATTACKS_H
 #define ATTACKS_H
 #include "../include/board.h"
-
+#define BOARD_INDEX(row, col) ((col) + (ROWS * (row)))
 bitboard computePawnAttack(int row, int col, int side);
+bitboard computeKingAttack(int row, int col);
 #endif
--- src/attacks.c
+++ src/attacks.c
@@ -1,4 +1,5 @@
 #include "../include/attacks.h"
+#define DEBBUG 1
 
 // THIS CONST REPRESENT THE BOARD BELOW THEM
 const bitboard NOT_COL_0 = 18374403900871474942ULL;
@@ -21,21 +22,37 @@ const bitboard NOT_COL_7 = 9187201950435737471ULL;
 // 0 1 1 1 1 1 1 1
 // 0 1 1 1 1 1 1 1
 
+// This follows the logic above but with rows
+const bitboard NOT_ROW_0 = 18446744073709551360ULL;
+const bitboard NOT_ROW_7 = 72057594037927935ULL;
+
 bitboard computePawnAttack(int row, int col, int side) {
-  bitboard pawnAttack = 0;
-  uint64_t mask = (uint64_t)1 << (col + ROWS * row);
+  bitboard attack = 0;
+  uint64_t mask = (uint64_t)1 << BOARD_INDEX(row, col);
   // side == 1 means black
   if (side) {
     if (mask & NOT_COL_7)
-      pawnAttack |= (mask >> 7);
+      attack |= (mask >> 7);
     if (mask & NOT_COL_0)
-      pawnAttack |= (mask >> 9);
+      attack |= (mask >> 9);
   } else {
     if (mask & NOT_COL_0)
-      pawnAttack |= (mask << 7);
+      attack |= (mask << 7);
     if (mask & NOT_COL_7)
-      pawnAttack |= (mask << 9);
+      attack |= (mask << 9);
   }
-  printBitboard(pawnAttack | mask);
-  return pawnAttack;
+  if (DEBBUG) {
+    printBitboard(attack | mask);
+  }
+  return attack;
+}
+
+bitboard computeKingAttack(int row, int col) {
+  bitboard attack = 0;
+  uint64_t mask = (uint64_t)1 << BOARD_INDEX(row, col);
+
+  if (DEBBUG) {
+    printBitboard(attack | mask);
+  }
+  return attack;
 }
