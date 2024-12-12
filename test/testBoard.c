7768663fdb6dcd121c455988a2ceff41fd7363f5
jonathan <joni7840850@gmail.com>
2024-12-12 12:35:53 -0600

    Add case numeration for error message

--- test/testBoard.c
+++ test/testBoard.c
@@ -5,13 +5,13 @@ int testSetBitboard0() {
   bitboard bb = 1234;
   setBitboard0(&bb);
   if (bb) {
-    printf("Error in function setBitboard0 \n");
+    printf("Error in function setBitboard0 case 0\n");
     return 0;
   }
   bb = 0;
   setBitboard0(&bb);
   if (bb) {
-    printf("Error in function setBitboard0 \n");
+    printf("Error in function setBitboard0 case 1\n");
     return 0;
   }
   return 1;
@@ -21,25 +21,25 @@ int testPlaceBitValue() {
   bitboard bb = 0;
   placeBitValue(0, 0, &bb);
   if (bb) {
-    printf("Error in function setbitvalue \n");
+    printf("Error in function setbitvalue case 0 \n");
     return 0;
   }
   bb = 0;
   placeBitValue(0, 1, &bb);
   if (bb != 1ULL) {
-    printf("Error in function setbitvalue \n");
+    printf("Error in function setbitvalue case 1 \n");
     return 0;
   }
   bb = 1;
   placeBitValue(0, 0, &bb);
   if (bb) {
-    printf("Error in function setbitvalue \n");
+    printf("Error in function setbitvalue case 2\n");
     return 0;
   }
   bb = 0;
   placeBitValue(3, 1, &bb);
   if (bb != 8) {
-    printf("Error in function setbitvalue \n");
+    printf("Error in function setbitvalue case 3\n");
     return 0;
   }
   return 1;
