2e75687127d43abe9285922196f80f310df93098
jonathan <joni7840850@gmail.com>
2024-12-18 11:31:33 -0600

    Change (1ULL << n) for a macro

--- test/testAttacks.c
+++ test/testAttacks.c
@@ -1,6 +1,7 @@
 #include "test.h"
 #include <stdio.h>
 #define DEBUG 1
+#define BIT(n) (1ULL << (n))
 
 void showDiff(bitboard expected, bitboard result) {
   if (DEBUG) {
@@ -56,41 +57,46 @@ int testRookAttack(int bitIndex, bitboard board, bitboard result,
 }
 
 int testRookAttacks() {
+  // empty board
   bitboard bb = 0ULL;
   int c0 = testRookAttack(0, bb, 72340172838076926ULL, '0');
   int c1 = testRookAttack(7, bb, 9259542123273814143ULL, '1');
   int c2 = testRookAttack(56, bb, 18302911464433844481ULL, '2');
   int c3 = testRookAttack(63, bb, 9187484529235886208ULL, '3');
-  return c0 && c1 && c2 && c3;
+  int c4 = testRookAttack(20, bb, 1157442765423841296, '4');
+  // With blockers
+  bb = 0ULL + (1ULL << 1) + (1ULL << 8);
+  return c0 && c1 && c2 && c3 && c4;
 }
 
+#define BIT(n) (1ULL << (n))
+
 int testPawnAttacks() {
-  int c0 = testPawnAttack(9, 1, (1ULL << 0) + (1ULL << 2), '0');
+  int c0 = testPawnAttack(9, 1, BIT(0) + BIT(2), '0');
   int c1 = testPawnAttack(1, 1, 0, '1');
-  int c2 = testPawnAttack(0, 0, (1ULL << 9), '2');
-  int c3 = testPawnAttack(7, 0, (1ULL << 14), '3');
-  int c4 = testPawnAttack(56, 1, (1ULL << 49), '4');
-  int c5 = testPawnAttack(63, 1, (1ULL << 54), '5');
-  int c6 = testPawnAttack(54, 0, (1ULL << 63) + (1ULL << 61), '6');
+  int c2 = testPawnAttack(0, 0, BIT(9), '2');
+  int c3 = testPawnAttack(7, 0, BIT(14), '3');
+  int c4 = testPawnAttack(56, 1, BIT(49), '4');
+  int c5 = testPawnAttack(63, 1, BIT(54), '5');
+  int c6 = testPawnAttack(54, 0, BIT(63) + BIT(61), '6');
   return c1 && c0 && c3 && c2 && c4 && c5 && c6;
 }
 
 int testKnightAttacks() {
-  int c0 = testKnightAttack(0, (1ULL << 17) + (1ULL << 10), '0');
-  int c1 = testKnightAttack(7, (1ULL << 22) + (1ULL << 13), '1');
-  int c2 = testKnightAttack(56, (1ULL << 50) + (1ULL << 41), '2');
-  int c3 = testKnightAttack(63, (1ULL << 46) + (1ULL << 53), '3');
+  int c0 = testKnightAttack(0, BIT(17) + BIT(10), '0');
+  int c1 = testKnightAttack(7, BIT(22) + BIT(13), '1');
+  int c2 = testKnightAttack(56, BIT(50) + BIT(41), '2');
+  int c3 = testKnightAttack(63, BIT(46) + BIT(53), '3');
   return c0 && c1 && c2 && c3;
 }
 
 int testKingAttacks() {
-  int c0 = testKingAttack(0, (1ULL << 1) + (1ULL << 8) + (1ULL << 9), '0');
-  int c1 = testKingAttack(7, (1ULL << 6) + (1ULL << 15) + (1ULL << 14), '1');
-  int c2 = testKingAttack(56, (1ULL << 57) + (1ULL << 48) + (1ULL << 49), '2');
-  int c3 = testKingAttack(63, (1ULL << 62) + (1ULL << 55) + (1ULL << 54), '3');
+  int c0 = testKingAttack(0, BIT(1) + BIT(8) + BIT(9), '0');
+  int c1 = testKingAttack(7, BIT(6) + BIT(15) + BIT(14), '1');
+  int c2 = testKingAttack(56, BIT(57) + BIT(48) + BIT(49), '2');
+  int c3 = testKingAttack(63, BIT(62) + BIT(55) + BIT(54), '3');
   return c0 && c1 && c2 && c3;
 }
-
 void testAttacks() {
   int resultTestPawnAttack = testPawnAttacks();
   int resultTestKnightAttack = testKnightAttacks();
