44aae7f5b9f6addf847b7cbeabd2b3d88ccdea67
jonathan <joni7840850@gmail.com>
2024-12-18 09:13:21 -0600

    Move state outside function this way is pseudo random

--- src/random.c
+++ src/random.c
@@ -4,8 +4,8 @@
  * source :
  * https://www.youtube.com/watch?v=JjFYmkUhLN4&list=PLmN0neTso3Jxh8ZIylk74JpwfiWNI76Cs&index=13
  */
+unsigned int state = 1804289383;
 static unsigned int getRandom32UNumber() {
-  unsigned int state = 1804289383;
   unsigned int number = state;
   number ^= number << 13;
   number ^= number >> 17;
@@ -16,10 +16,10 @@ static unsigned int getRandom32UNumber() {
 
 static uint64_t getRandom64UNumber() {
   uint64_t n1, n2, n3, n4;
-  n1 = (uint64_t)(getRandom32UNumber() & 0xFFFF);
-  n2 = (uint64_t)(getRandom32UNumber() & 0xFFFF);
-  n3 = (uint64_t)(getRandom32UNumber() & 0xFFFF);
-  n4 = (uint64_t)(getRandom32UNumber() & 0xFFFF);
+  n1 = (uint64_t)(getRandom32UNumber()) & 0xFFFF;
+  n2 = (uint64_t)(getRandom32UNumber()) & 0xFFFF;
+  n3 = (uint64_t)(getRandom32UNumber()) & 0xFFFF;
+  n4 = (uint64_t)(getRandom32UNumber()) & 0xFFFF;
   return n1 | (n2 << 16) | (n3 << 32) | (n4 << 48);
 }
 
