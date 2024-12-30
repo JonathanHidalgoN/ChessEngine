40ec45d7a936027d0380eb33f62f332a4de933ba
jonathan <joni7840850@gmail.com>
2024-12-30 10:10:23 -0600

    Add piece list implementation

--- src/board/pieceList.c
+++ src/board/pieceList.c
@@ -1,3 +1,8 @@
-#include "../../include/board/pieceList.h"
+#include "../../include//pieceList.h"
 
-pieceList generatePieceList(chessBoard *board) {}
+pieceList generatePieceList(chessBoard *board) {
+  for (int i = 0; i < NUMBEROFCOLORS; i++) {
+    for (int j = 0; j < NUMBEROFDIFFERENTPIECES; j++) {
+    }
+  }
+}
