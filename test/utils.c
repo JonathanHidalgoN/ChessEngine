66f38fab13897a83bbbaa9c884cfc7402b213c17
jonathan <joni7840850@gmail.com>
2025-01-01 14:15:25 -0600

    Add hisotry and game state utils functions

--- test/utils.c
+++ test/utils.c
@@ -51,3 +51,32 @@ piece createPiece(int bitIndex, int side, int type) {
   piece result = {side, type, bitIndex};
   return result;
 }
+
+int areGameStatesEqual(gameState *expected, gameState *result) {
+  return expected->playingSide == result->playingSide &&
+         expected->castlingCode == result->castlingCode &&
+         expected->halfMoveCounter == result->halfMoveCounter &&
+         expected->enPassantCode == result->enPassantCode &&
+         expected->fullMoveCounter == result->fullMoveCounter &&
+         expected->zobristKey == result->zobristKey &&
+         expected->phaseValue == result->phaseValue;
+}
+
+void printGameState(gameState *state) {
+  printf("playingSide: %d\ncastlingCode: %d\nhalfMoveCounter: %d\n"
+         "enPassantCode: %d\nfullMoveCounter: %d\nzobristKey: %lu\n"
+         "phaseValue: %d\n",
+         state->playingSide, state->castlingCode, state->halfMoveCounter,
+         state->enPassantCode, state->fullMoveCounter, state->zobristKey,
+         state->phaseValue);
+}
+
+gameState createGameState(int playingSide, int castlingCode,
+                          int halfMoveCounter, int enPassantCode,
+                          int fullMoveCounter, uint64_t zobristKey,
+                          int phaseValue) {
+  gameState result = {playingSide,   castlingCode,    halfMoveCounter,
+                      enPassantCode, fullMoveCounter, zobristKey,
+                      phaseValue};
+  return result;
+}
