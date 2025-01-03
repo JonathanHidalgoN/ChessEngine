68f65c1ea66c75aaa2c86729f1c76ecdd22059ca
jonathan <joni7840850@gmail.com>
2025-01-02 22:31:57 -0600

    Add optimization

--- src/board/pieceList.c
+++ src/board/pieceList.c
@@ -6,11 +6,10 @@ void updatePieceList(pieceList *pieceList, bitBoardsList *bitBoardsList) {
   for (int i = 0; i < NUMBEROFCOLORS; i++) {
     for (int j = 0; j < NUMBEROFDIFFERENTPIECES; j++) {
       bitboard bb = bitBoardsList->pieces[i][j];
-      for (int k = 0; k < NUMBEROFSQUARES; k++) {
-        bitboard mask = 1ULL << k;
-        if (mask & bb) {
-          pieceList->pieces[k] = j;
-        }
+      while (bb) {
+        int k = __builtin_ctzll(bb);
+        bb &= bb - 1;
+        pieceList->pieces[k] = j;
       }
     }
   }
