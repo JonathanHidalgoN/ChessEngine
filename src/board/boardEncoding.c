b8d528919b070a606d6e089222556e991a38410e
jonathan <joni7840850@gmail.com>
2025-01-02 22:22:12 -0600

    Add prototipe for zobris encoding

--- src/board/boardEncoding.c
+++ src/board/boardEncoding.c
@@ -1,4 +1,5 @@
 #include "../../include/board/boardEncoding.h"
+#include <stdint.h>
 
 void fillZobristRandoms(zobristRandoms *randoms) {
   int i;
@@ -19,4 +20,26 @@ void fillZobristRandoms(zobristRandoms *randoms) {
     randomNumber = getRandom64UNumber();
     randoms->passantRandoms[i] = randomNumber;
   }
+}
+
+uint64_t computeZobristFromBoard(zobristRandoms *randoms,
+                                 bitBoardsList *bitBoardsList, int side,
+                                 int castlingCode, int passantCode) {
+  uint64_t key = 0ULL;
+  int i, j, k;
+  for (i = 0; i < NUMBEROFCOLORS; i++) {
+    for (j = 0; j < NUMBEROFDIFFERENTPIECES; j++) {
+      for (k = 0; k < NUMBEROFSQUARES; k++) {
+        uint64_t mask = 1ULL << k;
+        if (mask & bitBoardsList->pieces[i][j]) {
+          int index = (int)i * j * k;
+          key ^= randoms->pieceRandoms[index];
+        }
+      }
+    }
+  }
+  key ^= randoms->castlingRandoms[castlingCode];
+  key ^= randoms->sidesRandoms[side];
+  key ^= randoms->passantRandoms[passantCode];
+  return key;
 }
