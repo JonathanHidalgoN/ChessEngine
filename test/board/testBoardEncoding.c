714a7447b5ecbbfb6df78148ec15ae840726d4a3
jonathan <joni7840850@gmail.com>
2025-01-06 13:34:27 -0600

    Add basic zobrist board test

--- test/board/testBoardEncoding.c
+++ test/board/testBoardEncoding.c
@@ -1,15 +1,24 @@
 #include "../../include/board/boardEncoding.h"
 #include "../test.h"
+#include <stdint.h>
 
 int testComputeZobristC1(zobristRandoms *randoms) {
+  uint64_t expectedKey, resultKey;
   bitBoardsList bbl;
   cleanBitBoardList(&bbl);
   int side = WHITE;
   int castlingCode = 0;
   int passantCode = 0;
-  uint64_t key =
+  bbl.pieces[WHITE][PAWN] = BIT(0);
+  expectedKey =
       computeZobristFromBoard(randoms, &bbl, side, castlingCode, passantCode);
-  printf("%lu\n", key);
+  side = BLACK;
+  resultKey =
+      computeZobristFromBoard(randoms, &bbl, side, castlingCode, passantCode);
+  if (expectedKey == resultKey) {
+    printf("Error in computeZobristFromBoard c1");
+    return 0;
+  }
   return 1;
 }
 
