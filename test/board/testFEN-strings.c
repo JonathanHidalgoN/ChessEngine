bc004f04b814cb2fd23c1f0dff888f489ddd6601
jonathan <joni7840850@gmail.com>
2025-01-08 18:46:59 -0600

    Add file to tets fen string

---
+++ test/board/testFEN-strings.c
@@ -0,0 +1,7 @@
+#include "../../include/board/FEN-strings.h"
+#include "../test.h"
+
+int testCheckValidFenString() {
+  char *validString1 =
+      "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+}
