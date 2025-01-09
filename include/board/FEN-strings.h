fe879d3679e3a05babfced4b9291cdcb7449de01
jonathan <joni7840850@gmail.com>
2025-01-09 17:36:22 -0600

    Change const definitions to extern

--- include/board/FEN-strings.h
+++ include/board/FEN-strings.h
@@ -1,11 +1,12 @@
+
 #ifndef FEN_STRINGS
 #define FEN_STRINGS
 
 #include "defs.h"
 #include "stdlib.h"
 #include <ctype.h>
-#define FEN_STRING_NUMBER_OF_VALID_POSITION_CHARACTERS 22
 
+#define FEN_STRING_NUMBER_OF_VALID_POSITION_CHARACTERS 21
 #define FEN_STRING_CASTLING_WHITE_QUEEN_SIDE 'Q'
 #define FEN_STRING_CASTLING_WHITE_KING_SIDE 'K'
 #define FEN_STRING_CASTLING_BLACK_QUEEN_SIDE 'q'
@@ -15,30 +16,14 @@
 #define FEN_STRING_LIMIT_HALF_MOVE 50
 #define FEN_STRING_LIMIT_FULL_MOVE 800
 
-const char FEN_STRING_VALID_POSITION_CHARACTERS
-    [FEN_STRING_NUMBER_OF_VALID_POSITION_CHARACTERS] = {
-        'p', 'n', 'b', 'q', 'k', 'r',           // Black pieces
-        'P', 'N', 'B', 'Q', 'K', 'R',           // White pieces
-        '/',                                    // Rank separator
-        '1', '2', '3', '4', '5', '6', '7', '8', // Empty squares
-};
-
-const char FEN_STRING_VALID_SIDE_CHARACTERS[NUMBEROFCOLORS] = {'w', 'b'};
-
-const char FEN_STRING_VALID_CASTLING_CHARACTERS
-    [FEN_STRING_NUMBER_CASTLING_CHARACTERS] = {
-        FEN_STRING_CASTLING_WHITE_QUEEN_SIDE,
-        FEN_STRING_CASTLING_WHITE_KING_SIDE,
-        FEN_STRING_CASTLING_BLACK_QUEEN_SIDE,
-        FEN_STRING_CASTLING_BLACK_KING_SIDE};
+extern const char FEN_STRING_VALID_POSITION_CHARACTERS
+    [FEN_STRING_NUMBER_OF_VALID_POSITION_CHARACTERS];
+extern const char FEN_STRING_VALID_SIDE_CHARACTERS[NUMBEROFCOLORS];
+extern const char
+    FEN_STRING_VALID_CASTLING_CHARACTERS[FEN_STRING_NUMBER_CASTLING_CHARACTERS];
 
 /*
- * This struct represent a FEN string where
- * string : is the full string
- * stringLen : len of the string
- * piecesPositions : int pair with the index where piece position starts and
- * ends sideToMove : index where side to is castling : int pair with index where
- * castling starts and ends and so on.
+ * This struct represents a FEN string.
  */
 typedef struct fenString {
   char *string;
@@ -51,10 +36,12 @@ typedef struct fenString {
   intPair fullMove;
 } fenString;
 
+void initFenString(char *string, int stringLen, fenString *fenString);
 int checkValidFenStringPart1(char *string, int start, int end);
 int checkValidFenStringPart2(char *string, int start);
 int checkValidFenStringPart3(char *string, int start, int end);
 int checkValidFenStringNumberVals(char *string, int lowIndex, int highIndex,
                                   int limit);
 int checkValidFenString(fenString *fenString);
-#endif // !FEN_STRINGS
+
+#endif // FEN_STRINGS
