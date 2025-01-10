141aece21909117d9080926185a43898c4516a1c
jonathan <joni7840850@gmail.com>
2025-01-09 22:37:50 -0600

    Add test case

--- test/board/testFEN-strings.c
+++ test/board/testFEN-strings.c
@@ -1,7 +1,18 @@
-#include "../../include/board/FEN-strings.h"
 #include "../test.h"
 
-int testCheckValidFenString() {
-  char *validString1 =
+int testInitFenString() {
+  int stringLen = 56;
+  char *validString =
       "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+  fenString fenString;
+  initFenString(validString, stringLen, &fenString);
+  printFenString(&fenString);
+  return 1;
+}
+
+void testFenString() {
+  int resultTestInitFenString = testInitFenString();
+  if (resultTestInitFenString) {
+    printf(GREEN "Tested FEN string successfully\n" RESET);
+  }
 }
