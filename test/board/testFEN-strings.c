7db18a458a5cb136420aabf54ebfce70acb646cf
jonathan <joni7840850@gmail.com>
2025-01-10 20:58:26 -0600

    Add some more test cases

--- test/board/testFEN-strings.c
+++ test/board/testFEN-strings.c
@@ -49,15 +49,15 @@ int testInitFenString() {
   return c1 && c2 && c3 && c4;
 }
 
-int testCaseCheckValidFenString(fenString *fenString, int expetingValid,
+int testCaseCheckValidFenString(fenString *fenString, int expectingValid,
                                 char testNumber) {
   int valid = checkValidFenString(fenString);
-  if (expetingValid && !valid) {
+  if (expectingValid && !valid) {
     printf(RED "Error, expecting valid FEN string but got invalid %c\n" RESET,
            testNumber);
     printFenString(fenString);
     return 0;
-  } else if (!expetingValid && valid) {
+  } else if (!expectingValid && valid) {
     printf(RED "Error, expecting invalid FEN string but got valid %c\n" RESET,
            testNumber);
     printFenString(fenString);
@@ -66,9 +66,56 @@ int testCaseCheckValidFenString(fenString *fenString, int expetingValid,
   return 1;
 }
 
+int testCheckValidFenString() {
+  int expectingValid = 1;
+  int stringLen = 56;
+  char *validString =
+      "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+  fenString result;
+  initFenString(validString, stringLen, &result);
+  int c0 = testCaseCheckValidFenString(&result, expectingValid, '0');
+
+  expectingValid = 1;
+  stringLen = 56;
+  validString = "--------/--------/8/8/8/8/--------/-------- w ---- - 0 0";
+  initFenString(validString, stringLen, &result);
+  int c1 = testCaseCheckValidFenString(&result, expectingValid, '0');
+
+  expectingValid = 1;
+  stringLen = 28;
+  validString = "8/8/8/8/8/8/8/8 w KQkq - 0 1";
+  initFenString(validString, stringLen, &result);
+  int c2 = testCaseCheckValidFenString(&result, expectingValid, '0');
+
+  expectingValid = 1;
+  stringLen = 87;
+  validString = "rnbqkbnr/pppppppp/pppppppp/pppppppp/pppppppp/pppppppp/"
+                "PPPPPPPP/RNBQKBNR w KQkq 10 10 11";
+  initFenString(validString, stringLen, &result);
+  int c3 = testCaseCheckValidFenString(&result, expectingValid, '0');
+
+  // Add invalid char "z" to get invalid FEN string
+  expectingValid = 0;
+  stringLen = 87;
+  validString = "znbqkbnr/pppppppp/pppppppp/pppppppp/pppppppp/pppppppp/"
+                "PPPPPPPP/RNBQKBNR w KQkq 10 10 11";
+  initFenString(validString, stringLen, &result);
+  int c4 = testCaseCheckValidFenString(&result, expectingValid, '0');
+  // Add invalid char "z" to get invalid FEN string at the end
+  expectingValid = 0;
+  stringLen = 87;
+  validString = "pnbqkbnr/pppppppp/pppppppp/pppppppp/pppppppp/pppppppp/"
+                "PPPPPPPP/RNBQKBNR w KQkq 10 10 1z";
+  initFenString(validString, stringLen, &result);
+  int c5 = testCaseCheckValidFenString(&result, expectingValid, '0');
+
+  return c0 && c1 && c2 && c3 && c4 && c5;
+}
+
 void testFenString() {
   int resultTestInitFenString = testInitFenString();
-  if (resultTestInitFenString) {
+  int resultCheckValidFenString = testCheckValidFenString();
+  if (resultTestInitFenString && resultCheckValidFenString) {
     printf(GREEN "Tested FEN string successfully\n" RESET);
   }
 }
