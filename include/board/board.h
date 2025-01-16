0c2ce5792b794f058fd63ed467727fa29442394a
jonathan <joni7840850@gmail.com>
2025-01-16 15:56:52 -0600

    Delete duplicate function declaration

--- include/board/board.h
+++ include/board/board.h
@@ -36,29 +36,4 @@ typedef struct board {
   pieceList pieceList;
 } board;
 
-/**
- Set all pieces of the board to empty board
-*/
-void cleanBitBoardList(bitBoardsList *bitBoardsList);
-/**
-  print the bitboard
- */
-void printBitboard(bitboard bb);
-/**
-  Place the val in the bitIndex on the Bitboard
- */
-void placeBitValue(int bitIndex, int val, bitboard *bb);
-/**
-  Init the bitBoardsList with the classic chess config
- */
-void initBitBoardsListStandarChess(bitBoardsList *bitBoardsList);
-/**
-  Create a string representation of the bitBoardsList
- */
-void placePieceRepresentationIntoBoardString(bitboard bb, char *br,
-                                             char symbol);
-/**
-  Print the bitBoardsList
- */
-void printChessBoard(bitBoardsList *bitBoardsList);
 #endif
