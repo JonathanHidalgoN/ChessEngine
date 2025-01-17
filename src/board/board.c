e62458e42b8e57770f962dab5fd20931ae4fcfc2
jonathan <joni7840850@gmail.com>
2025-01-17 15:59:07 -0600

    Update zobrist key when making a move in board

--- src/board/board.c
+++ src/board/board.c
@@ -4,12 +4,16 @@ void removePiece(board *board, int square, int side, int piece) {
   bitboard mask = ~(1ULL << square);
   board->bitBoardsList.pieces[side][piece] &= mask;
   board->pieceList.pieces[square] = EMPTY;
+  board->gameState.zobristKey ^=
+      board->zobristRandoms.pieceRandoms[square][side][piece];
 }
 
 void putPiece(board *board, int square, int side, int piece) {
   bitboard mask = 1ULL << square;
   board->bitBoardsList.pieces[side][piece] |= mask;
   board->pieceList.pieces[square] = piece;
+  board->gameState.zobristKey ^=
+      board->zobristRandoms.pieceRandoms[square][side][piece];
 }
 
 void movePiece(board *board, int square, int side, int piece) {
